613744701e8091cf673e55455cf8fa02c3b83de5
Aurora Scharff Walberg <aurora.walberg@inmeta.no>
2024-06-04 22:19:37 +0200

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,18 @@
+import './globals.css';
+import { Inter } from 'next/font/google';
+import type { Metadata } from 'next';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  description: 'Next 14 Starter',
+  title: 'Next 14 starter with ESLint, Prettier, and Tailwind',
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
