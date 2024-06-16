c43800e351679a763138901e081e00ed25171a4e
Aurora Scharff Walberg <aurora.walberg@inmeta.no>
2024-06-16 23:40:08 +0200

    Add error outline to input fields

--- components/ui/Input.tsx
+++ components/ui/Input.tsx
@@ -8,7 +8,7 @@ type Props = {
 export default function Input({ error, ...otherProps }: Props & React.InputHTMLAttributes<HTMLInputElement>) {
   return (
     <div>
-      <input aria-describedby="error" {...otherProps} />
+      <input className={error ? 'outline outline-2 outline-destroy' : ''} aria-describedby="error" {...otherProps} />
       {error && <span className="text-destroy">{error}</span>}
     </div>
   );
--- components/ui/TextArea.tsx
+++ components/ui/TextArea.tsx
@@ -8,8 +8,8 @@ type Props = {
 export default function TextArea({ error, ...otherProps }: Props & React.TextareaHTMLAttributes<HTMLTextAreaElement>) {
   return (
     <div>
-      <textarea aria-describedby="error" {...otherProps} />
-      {error && <span className="text-destroy">{error}</span>}
+      <textarea className={error ? 'outline outline-2 outline-destroy' : ''} aria-describedby="error" {...otherProps} />
+      {error && <span className="font- text-destroy">{error}</span>}
     </div>
   );
 }
