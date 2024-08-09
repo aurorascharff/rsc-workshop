8164381e415b19a7cbaa74bef8e4844b393e2633
Aurora Scharff <aurora.sofie@gmail.com>
2024-08-09 09:56:57 +0200

    Remove suspense from layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import './globals.css';
 import { Inter } from 'next/font/google';
 import Image from 'next/image';
 import Link from 'next/link';
-import { Suspense } from 'react';
 import { Toaster } from 'react-hot-toast';
 import ContactList from '@/components/ContactList';
 import Search from '@/components/Search';
@@ -32,15 +31,13 @@ export default async function RootLayout({ children }: { children: React.ReactNo
         <div className="flex w-full flex-col sm:flex-row">
           <div className="flex h-1/3 w-full flex-col border-r border-gray bg-gray-light sm:h-auto sm:w-[16rem] md:w-[22rem]">
             <Toaster position="top-right" />
-            <Suspense>
-              <div className="flex items-center gap-2 border-b border-gray px-8 py-4">
-                <Search />
-                <form action={createEmptyContact}>
-                  <SubmitButton theme="secondary">New</SubmitButton>
-                </form>
-              </div>
-              <ContactList contacts={contacts} />
-            </Suspense>
+            <div className="flex items-center gap-2 border-b border-gray px-8 py-4">
+              <Search />
+              <form action={createEmptyContact}>
+                <SubmitButton theme="secondary">New</SubmitButton>
+              </form>
+            </div>
+            <ContactList contacts={contacts} />
             <div className="m-0 hidden flex-row items-center gap-2 border-t border-t-gray px-8 py-4 font-medium sm:flex">
               <Link className="flex items-center gap-2 text-black no-underline" href={routes.home()}>
                 <Image width={30} height={30} src={Logo} alt="" />
