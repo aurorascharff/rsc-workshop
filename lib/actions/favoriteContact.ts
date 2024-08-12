91422e10f78f8951518ba613004fa32dce9b6e07
Aurora Scharff <aurora.sofie@gmail.com>
2024-08-12 21:04:43 +0200

    Add slow to all mutations

--- lib/actions/favoriteContact.ts
+++ lib/actions/favoriteContact.ts
@@ -2,9 +2,12 @@
 
 import { revalidatePath } from 'next/cache';
 import { prisma } from '@/db';
+import { slow } from '@/utils/slow';
 import { routes } from '@/validations/routeSchema';
 
 export async function favoriteContact(contactId: string, isFavorite: boolean) {
+  await slow();
+
   await prisma.contact.update({
     data: {
       favorite: !isFavorite,
--- lib/actions/updateContact.ts
+++ lib/actions/updateContact.ts
@@ -4,6 +4,7 @@ import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 
 import { prisma } from '@/db';
+import { slow } from '@/utils/slow';
 import type { ContactSchemaErrorType, ContactSchemaType } from '@/validations/contactSchema';
 import { contactSchema } from '@/validations/contactSchema';
 import { routes } from '@/validations/routeSchema';
@@ -14,6 +15,8 @@ type State = {
 };
 
 export async function updateContact(contactId: string, _prevState: State, formData: FormData): Promise<State> {
+  await slow();
+
   const contact = Object.fromEntries(formData);
   const result = contactSchema.safeParse(contact);
 
