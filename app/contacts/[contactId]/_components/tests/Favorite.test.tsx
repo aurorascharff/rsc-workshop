e0ec1db1825137086a32c5fb097b7e8e67cb58f6
Aurora Scharff <aurora.sofie@gmail.com>
2024-08-15 17:59:47 +0200

    Update tests

--- app/contacts/[contactId]/_components/tests/Favorite.test.tsx
+++ app/contacts/[contactId]/_components/tests/Favorite.test.tsx
@@ -31,16 +31,22 @@ describe('Favorite', () => {
     expect(favoriteButton).toHaveAccessibleName('Remove from favorites');
   });
 
-  it('updates when button is clicked and calls the correct function', () => {
+  it('updates when button is clicked and calls the correct function', async () => {
     render(<Favorite contact={{ favorite: false, id: '0' } as Contact} />);
 
     const favoriteButton = screen.getByRole('button');
     expect(favoriteButton).toHaveTextContent('☆');
 
-    act(() => {
+    await act(async () => {
       fireEvent.click(favoriteButton);
     });
-    expect(favoriteButton).toHaveTextContent('★');
+    expect(favoriteButton).toHaveTextContent('☆');
+
+    // When we don´t await the event, we assert the UI before the optimistic update has "rolled back" to the original state (because the server state did not update).
+    // act(() => {
+    //   fireEvent.click(favoriteButton);
+    // });
+    // expect(favoriteButton).toHaveTextContent('★');
 
     expect(mockFavoriteContact).toHaveBeenCalledWith('0', false);
   });
