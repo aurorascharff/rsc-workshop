7bc8faa1cf90dc24a596d34a5d62202fd90f9626
Aurora Scharff <aurora.sofie@gmail.com>
2024-08-18 21:36:45 +0200

    Format

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,5 @@
-import { PrismaClient, User } from '@prisma/client';
+import { PrismaClient } from '@prisma/client';
+import type { User } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
