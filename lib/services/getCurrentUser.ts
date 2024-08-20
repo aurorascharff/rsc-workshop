7f397a606e4b3e7fb615f042e8cce5546ec68a48
Aurora Scharff <aurora.sofie@gmail.com>
2024-08-20 20:18:14 +0200

    Randomize current user

--- lib/services/getCurrentUser.ts
+++ lib/services/getCurrentUser.ts
@@ -4,5 +4,5 @@ import { prisma } from '@/db';
 
 export async function getCurrentUser() {
   const users = await prisma.user.findMany();
-  return users[1];
+  return users[Math.floor(Math.random() * 2)];
 }
