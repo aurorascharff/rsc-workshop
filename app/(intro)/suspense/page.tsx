789de2a9e47139771c35e9257b7808c2f53fc2b8
Aurora Scharff <aurora.sofie@gmail.com>
2024-09-13 14:28:24 +0200

    Remove /intro route into a group

--- app/(intro)/suspense/page.tsx
+++ app/(intro)/suspense/page.tsx
@@ -5,7 +5,7 @@ export default function SuspensePage() {
   return (
     <div className="flex flex-col gap-8">
       <h1>Suspense</h1>
-      <LinkButton href="/intro/suspense/slow">Slow page</LinkButton>
+      <LinkButton href="/suspense/slow">Slow page</LinkButton>
     </div>
   );
 }
